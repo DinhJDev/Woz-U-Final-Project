5ec3e31632599a125cb448c76f07fa2686a26bac
Jason Dinh <dinhjdev@gmail.com>
2021-10-19 15:08:19 -0500

    add: Account Model
    - Added Account Model

---
+++ src/main/java/com/wozu/hris/models/Account.java
@@ -0,0 +1,65 @@
+package com.wozu.hris.models;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private Date createdAt;
+    private Date updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
