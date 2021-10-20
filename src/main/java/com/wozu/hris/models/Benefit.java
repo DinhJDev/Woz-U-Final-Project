1bcc4dcd79ef63475414c0fa3042f87ea12d3c3e
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2021-10-20 10:06:02 -0500

    added Benefits, configured README, added assets

---
+++ src/main/java/com/wozu/hris/models/Benefit.java
@@ -0,0 +1,102 @@
+package com.wozu.hris.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/* ------------------------------------------------------------------
+**  —— Benefits Model
+**  —— Created by: Chetachi Ezikeuzor
+** -----------------------------------------------------------------*/
+
+/* ------------------------------------------------------------------
+**  —— TABLE OF CONTENTS
+**  —— 24. Attributes
+**  —— 41. Create/Update
+**  —— 54. Getters
+**  —— 78. Setters
+** -----------------------------------------------------------------*/
+
+@Entity
+@Table(name="benefits")
+public class Benefit {
+
+    /*----------------------------------------------------------------
+    ATTRIBUTES
+    ----------------------------------------------------------------*/
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String description;
+
+    private Date createdAt;
+    private Date updatedAt;
+
+    /*----------------------------------------------------------------
+    CREATE AND UPDATE
+    ----------------------------------------------------------------*/
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+
+    /*----------------------------------------------------------------
+    GETTERS
+    ----------------------------------------------------------------*/
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public Date getCreatedAt(){
+        return createdAt;
+    }
+
+    public Date getUpdatedAt(){
+        return updatedAt;
+    }
+
+    /*----------------------------------------------------------------
+    SETTERS
+    ----------------------------------------------------------------*/
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
