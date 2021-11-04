1ffccf7cf6ecc323a4647aa4c13955cd5c463dae
Jason Dinh <dinhjdev@gmail.com>
2021-11-04 15:26:27 -0500

    update: small fix to match with main

--- backend/src/main/java/com/wozu/hris/models/Performance.java
+++ backend/src/main/java/com/wozu/hris/models/Performance.java
@@ -32,7 +32,7 @@ public class Performance {
         this.updatedAt = new Date();
     }
 
-    public Performance(String comment, Employee reviewee, Employee reviewer) {
+    public Performance(String comment, Employee reviewer, Employee reviewee) {
         this.comment = comment;
         this.reviewer = reviewer;
         this.reviewee = reviewee;
