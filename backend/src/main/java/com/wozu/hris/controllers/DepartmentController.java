a4dc825caaaa62faa57b24f0926db41ed3570786
Jason Dinh <dinhjdev@gmail.com>
2021-11-06 12:48:56 -0500

    update: EmployeeController

---
+++ backend/src/main/java/com/wozu/hris/controllers/DepartmentController.java
@@ -0,0 +1,11 @@
+package com.wozu.hris.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/department")
+public class DepartmentController {
+}
--- backend/src/main/java/com/wozu/hris/models/Performance.java
+++ backend/src/main/java/com/wozu/hris/models/Performance.java
@@ -77,4 +77,5 @@ public class Performance {
 
     public void setReviewee(Employee reviewee) {
         this.reviewee = reviewee;
+    }
 }
